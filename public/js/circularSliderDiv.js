c6b28852478f8526b262c755dae9d4f3d5ca3df9
Marina <marina.iovbak@gmail.com>
2019-01-07 17:56:33 +0600

    Slider titles added next step add links change titles style fix slider arrows position

--- public/js/circularSliderDiv.js
+++ public/js/circularSliderDiv.js
@@ -1,35 +1,31 @@
 var imagesPathsArr = ["images/widgets/chick1.png", "images/widgets/swill.png", "images/widgets/chick2.jpg", "images/widgets/appointment.jpg",];
-// var imagesTitle = ["Gesichtspflege", "Hand und Fusspflege", "Uber mich", "Termin"];
+var imagesTitle = ["Gesichtspflege", "Hand und Fusspflege", "Uber mich", "Termin"];
 const slidersLength = 3;
 var currentSlides = [];
 
 function createSliderOnLoad () {
     for (var i = 0; i < slidersLength; i++) {
-       currentSlides[i] = document.createElement('div');
-       currentSlides[i].classList.add("sliderImg");
-       currentSlides[i].style.backgroundImage = "url(" + (imagesPathsArr[i]) + ")"; 
-       result.appendChild(currentSlides[i]);
+      result.appendChild(createSliderItem(i));
     }
     return;
 };
 
 next.onclick = function next () {
-       currentSlides[slidersLength] = document.createElement('div');
-       currentSlides[slidersLength].classList.add("sliderImg");     
-       currentSlides[slidersLength].style.backgroundImage = "url(" + (imagesPathsArr[slidersLength]) + ")";
-       result.appendChild(currentSlides[slidersLength]);
+       result.appendChild(createSliderItem(slidersLength));
        shiftArrayLeft(imagesPathsArr);
-       result.removeChild(currentSlides.shift());
+       shiftArrayLeft(imagesTitle);
+       result.removeChild(currentSlides.shift()); // go left -> delete first 
     };
 
 prev.onclick = function prev () {
-       currentSlides.unshift(document.createElement('div'));
-       currentSlides[0].classList.add("sliderImg");
-       currentSlides[0].style.backgroundImage = "url(" + (imagesPathsArr[slidersLength]) + ")";
-       result.insertBefore(currentSlides[0], result.firstChild);
+       currentSlides.unshift(document.createElement('div')); // go right -> insert first
        shiftArrayRight(imagesPathsArr);
+       shiftArrayRight(imagesTitle);
+       createSliderItem(0);
+       result.insertBefore(currentSlides[0], result.firstChild);
        result.removeChild(currentSlides[slidersLength]);
     };
+
 
 
 function shiftArrayLeft(arr) {
@@ -45,7 +41,23 @@ function shiftArrayRight(arr) {
     return arr;
 };
 
-
+function createSliderItem(index) {
+    // create wrapper div
+    currentSlides[index] = document.createElement("div");
+    currentSlides[index].classList.add("grid_item");
+    // create img div
+    var sliderImg = document.createElement("div");
+    sliderImg.classList.add("grid_img");
+    sliderImg.style.backgroundImage = "url(" + (imagesPathsArr[index]) + ")";
+    // create img title div
+    var sliderImgTitle = document.createElement("div");
+    sliderImgTitle.classList.add("grid_title");
+    sliderImgTitle.innerHTML = imagesTitle[index];
+    // place img and title inside wrapper div
+    currentSlides[index].appendChild(sliderImg);
+    currentSlides[index].appendChild(sliderImgTitle);
+    return currentSlides[index];
+};
 
 
 window.onload = createSliderOnLoad();
